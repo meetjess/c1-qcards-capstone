5cf143d18b65c01642398a6571297361f2de839b
meetjess <146796769+meetjess@users.noreply.github.com>
2024-06-01 21:50:25 -0700

    Update dashboard.jsx

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -8,7 +8,7 @@ export default function Dashboard () {
 				<p>Welcome to Q Cards! This simple web app helps you create multiple-choice question cards to aid your studying. Easily add questions, set correct answers, and use your custom cards to test your knowledge. Perfect for exam preparation and reinforcing new concepts. Get started with Q Cards and streamline your study sessions.</p>
 			</div>
 			<div className="existingcard-breakdown">
-				<p>if the user has a certain number of cards, this will show a unique call to action</p>
+				<p>This was created as a capstone project for Correlation One's Software Development training program and will continue evolving beyond my graduation. Thank you for stopping by! - Jess A.</p>
 			</div>
 		</div>
 		</>
